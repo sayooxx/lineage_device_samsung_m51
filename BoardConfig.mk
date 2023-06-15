7ebff7327a002bac6c572918e3636a9606a3fd71
Haky86 <hakamelassouad@gmail.com>
2023-06-15 16:33:13 +0200

    m51: Initial commit
    Change-Id: Ibb8889c6e59b2a8e56d881572e692ecd85a794a2

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include device/samsung/sm6150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/m51
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := m51
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Board
+TARGET_BOARD_NAME := SRPSF18B003
+
+# Kernel
+BOARD_HEADER_VERSION := 2
+TARGET_KERNEL_CONFIG := m51_defconfig
+
+# Get non-open-source specific aspects
+include vendor/samsung/m51/BoardConfigVendor.mk
