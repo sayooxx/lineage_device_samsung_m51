1775e68bb4a69fb88f0b0eb90d75fc38dda40f32
Haky86 <hakamelassouad@gmail.com>
2023-07-12 17:02:44 +0200

    m51: Import some seccomp policy configs from m51
    * Correct /vendor/etc/seccomp_policy path.
    * Since we're still depending on Android 11 blobs, we need those 2 configs to fix audio and camera stuff.
    * Taken from m51nsxx-user 11 RP1A.200720.012 M515FXXS4CUL1 release-keys.
    
    Change-Id: I55bf3ba557b0174ecefa0c6a5d26b3a93033a46e

--- device.mk
+++ device.mk
@@ -49,12 +49,12 @@ PRODUCT_PACKAGES += \
 
 # Seccomp Policy
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
+    $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/codec2.vendor.base.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.base.policy \
     $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
     $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/mediaextractor_sec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor_sec.policy \
-    $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/samsung.software.media.c2-base-policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/samsung.software.media.c2-base-policy \
-    $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/samsung.software.media.c2-ext-policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/samsung.software.media.c2-ext-policy \
     $(DEVICE_PATH)/rootdir/vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy \
 
 # Soong Namespaces
